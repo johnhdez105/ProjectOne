a3cfd3bc2a7548f45c7636c0ffcf5df1713785ff
johnhdez105 <johnhdez105@gmail.com>
2021-10-09 20:20:12 -0500

    Fix html for video stream and add eslint

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,6 +8,6 @@ requestUserMedia($self.constraints);
 
 async function requestUserMedia(constraints) {
   const video = document.querySelector('#self');
-  $self.stream = await navigator.mediaDevices .getUserMedia(constraints);
+  $self.stream = await navigator.mediaDevices.getUserMedia(constraints);
   video.srcObject = $self.stream;
 }
