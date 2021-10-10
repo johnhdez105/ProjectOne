65ecb220325a2874144c2eded777832af70eeeb1
johnhdez105 <johnhdez105@gmail.com>
2021-10-10 16:22:06 -0500

    Set up for peer connection and fixed search bar position

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const io = require('socket.io');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
