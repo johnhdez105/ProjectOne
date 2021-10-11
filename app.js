83d3ce3ec9ab88c8818c61bc18743c285b2dcc2e
johnhdez105 <johnhdez105@gmail.com>
2021-10-11 02:52:30 -0500

    Complete all signaling-channel events and Randomly generate correct namespaces

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-'use strict';
-
-const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
+const express = require('express');
+const io = require('socket.io')();
 const logger = require('morgan');
-const io = require('socket.io');
+const path = require('path');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,5 +17,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const namespaces = io.of(/^\/[0-9]{6}$/);
+
+namespaces.on('connection', function (socket) {
+  const namespace = socket.nsp;
+  socket.broadcast.emit('connected peer');
+
+});
 
 module.exports = { app, io };
