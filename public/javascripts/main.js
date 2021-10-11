e5e2d86a36af23983deb0b914496552172450d8a
johnhdez105 <johnhdez105@gmail.com>
2021-10-11 03:04:47 -0500

    Set up politeness and basic RTC callbacks

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,11 @@
 
 const $self = {
   rtcConfig: null,
-  constraints: { audio: false, video: true }
+  constraints: { audio: false, video: true },
+  isPolite: false,
+  isMakingOffer: false,
+  isIgnoringOffer: false,
+  isSettingRemoteAnswerPending: false
 };
 
 const $peer = {
@@ -27,9 +31,42 @@ button.addEventListener('click', joinCall);
 
 function joinCall() {
   sc.open();
+  registerRtcEvents($peer);
+  establishCallFeatures($peer);
 }
 function leaveCall() {
   sc.close();
+}
+
+function establishCallFeatures(peer) {
+  peer.connection
+    .addTrack($self.stream.getTracks()[0],
+      $self.stream);
+}
+
+function registerRtcEvents(peer) {
+  peer.connection
+    .onnegotiationneeded = handleRtcNegotiation;
+  peer.connection
+    .onicecandidate = handleIceCandidate;
+  peer.connection
+    .ontrack = handleRtcTrack;
+}
+
+async function handleRtcNegotiation() {
+  console.log('RTC negotiation needed...');
+
+  $self.isMakingOffer = true;
+  await $peer.connection.setLocalDescription();
+  sc.emit('signal', { description:
+    $peer.connection.localDescription });
+  $self.isMakingOffer = false;
+}
+function handleIceCandidate({ candidate }) {
+  sc.emit('signal', { candidate:
+    candidate });
+}
+function handleRtcTrack() {
 }
 
 function registerScEvents () {
@@ -39,16 +76,22 @@ function registerScEvents () {
   sc.on('disconnected peer', handleScDisconnectedPeer);
 }
 function handleScConnect() {
-  console.log('Connected to signaling channel')
+  console.log('Connected to signaling channel');
 }
 function handleScConnectedPeer() {
-  console.log('Heard connected peer event')
+  console.log('Heard connected peer event');
+  $self.isPolite = true;
 }
 function handleScDisconnectedPeer(){
-  console.log('Heard disconnected peer event')
+  console.log('Heard disconnected peer event');
 }
-async function handleScSignal(){
-  console.log('Heard signal event')
+async function handleScSignal({ description, candidate }) {
+  console.log('Heard signal event!');
+  if (description) {
+    console.log('Received SDP Signal:', description);
+  } else if (candidate) {
+    console.log('Received ICE candidate:', candidate);
+  }
 }
 
 function prepareNamespace(hash, set_location) {
